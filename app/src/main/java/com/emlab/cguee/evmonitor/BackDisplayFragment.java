928a769e08251f600c685e91f944b7f990c56579
Heiru Wu <wuheiru.5203@gmail.com>
2014-09-03 15:58:07 +0800

    Initial commit

---
+++ app/src/main/java/com/emlab/cguee/evmonitor/BackDisplayFragment.java
@@ -0,0 +1,163 @@
+package com.emlab.cguee.evmonitor;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+/**
+ * Created by hrw on 14/9/3.
+ */
+public class BackDisplayFragment extends Fragment implements SurfaceHolder.Callback {
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private Socket socket;
+    private ServerSocket serverSocket;
+    private InputStream inputStream;
+    private BufferedInputStream bufferedInputStream;
+    private MediaCodec mediaCodec;
+    private BufferedOutputStream bufferedOutputStream;
+    private static final String TAG = "EVmoniterActivity";
+    private byte[] temp;
+    private boolean isCon = false;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_backdisplay,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        surfaceView = (SurfaceView)getView().findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        VideoCodecInit();
+        temp = new byte[1382400];
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    serverSocket = new ServerSocket(2025);
+                    socket = serverSocket.accept();
+                    Log.w(TAG, "Socket connected");
+                    inputStream = socket.getInputStream();
+                    bufferedInputStream = new BufferedInputStream(inputStream);
+                    Log.w(TAG,"thread finished");
+                    isCon = true;
+                } catch (IOException e) {
+                    Log.w(TAG,e.toString());
+                }
+            }
+        }).start();
+        while(!isCon){
+
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(isCon){
+                    try {
+                        bufferedInputStream.read(temp,0,temp.length);
+                        offerDecoder(temp,0);
+                    } catch (IOException e) {
+                        Log.w(TAG,e.toString());
+                    }
+                }
+            }
+        }).start();
+    }
+    public void VideoCodecInit() {
+        mediaCodec = MediaCodec.createDecoderByType("video/avc");
+        MediaFormat mediaFormat = MediaFormat.createVideoFormat("video/avc", 1280, 720);
+        mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, 2500000);
+        mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 20);
+        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 5);
+        mediaCodec.configure(mediaFormat,null , null, 0);
+        mediaCodec.start();
+    }
+
+
+    // called from Camera.setPreviewCallbackWithBuffer(...) in other class
+    public void offerDecoder(byte[] input1,int offset) {
+        try {
+            Log.w("AVC Codec","Start decode");
+            ByteBuffer[] inputBuffers = mediaCodec.getInputBuffers();
+            ByteBuffer[] outputBuffers = mediaCodec.getOutputBuffers();
+            int inputBufferIndex = mediaCodec.dequeueInputBuffer(-1);
+            if (inputBufferIndex >= 0) {
+                ByteBuffer inputBuffer = inputBuffers[inputBufferIndex];
+                inputBuffer.clear();
+                inputBuffer.put(input1, offset, input1.length);
+                mediaCodec.queueInputBuffer(inputBufferIndex, 0, input1.length, 0, 0);
+            }
+
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+            int outputBufferIndex = mediaCodec.dequeueOutputBuffer(bufferInfo, 0);
+            while (outputBufferIndex >= 0) {
+                ByteBuffer outputBuffer = outputBuffers[outputBufferIndex];
+                byte[] outData = new byte[bufferInfo.size];
+                outputBuffer.get(outData);
+//                if (bufferInfo.offset != 0)
+//                {
+//                    bufferedOutputStream.write(outData, bufferInfo.offset, outData.length
+//                            - bufferInfo.offset);
+//                }
+//                else
+//                {
+//                    bufferedOutputStream.write(outData, 0, outData.length);
+//                }
+//                    bufferedOutputStream.write(outData, 0, outData.length);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(outData, 0, outData.length);
+                Canvas canvas = surfaceHolder.lockCanvas();
+                canvas.drawBitmap(bitmap, 0, 0, null);
+                surfaceHolder.unlockCanvasAndPost(canvas);
+                Log.i("AvcEncoder", outData.length + " bytes written");
+                mediaCodec.releaseOutputBuffer(outputBufferIndex, false);
+                outputBufferIndex = mediaCodec.dequeueOutputBuffer(bufferInfo, 0);
+
+            }
+        } catch (Throwable t) {
+            Log.w(TAG,"Codec: "+t.toString());
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}
---
+++ app/src/main/java/com/emlab/cguee/evmonitor/EVmoniterActivity.java
@@ -0,0 +1,140 @@
+package com.emlab.cguee.evmonitor;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.hardware.display.DisplayManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+
+
+public class EVmoniterActivity extends Activity implements SurfaceHolder.Callback, DisplayFragment.OnFragmentInteractionListener {
+    FragmentTransaction mFragmentTransaction = getFragmentManager()
+            .beginTransaction();
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            switch (position) {
+                case 0:
+                    fragment = DisplayFragment.newInstance("test1","test2");
+                    mFragmentTransaction.addToBackStack(null);
+                    break;
+                case 1:
+                    fragment = DisplayFragment.newInstance("test1","test2");
+                    mFragmentTransaction.addToBackStack(null);
+                    break;
+                case 2:
+//                    mFragmentTransaction.addToBackStack(null);
+                    break;
+                case 3:
+//                    mFragmentTransaction.addToBackStack(null);
+                    break;
+                case 4:
+//                    mFragmentTransaction.addToBackStack(null);
+                    break;
+            }
+
+            return fragment;
+        }
+
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return null;
+                case 1:
+                    return new String("Step 1");
+//                case 2:
+//                    return new String("Step 2");
+//                case 3:
+//                    return new String("Step 3");
+//                case 4:
+//                    return new String("Step 4");
+                default:
+                    return null;
+            }
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evmoniter);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.evmoniter, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+}
